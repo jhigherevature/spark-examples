19f1a153a0526f3e6f3238fabb4aebe88fc7c1d3
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2021-10-14 12:56:50 -0500

    Update GenericClass.java

--- java-environment/day16/src/generics/GenericClass.java
+++ java-environment/day16/src/generics/GenericClass.java
@@ -1,5 +1,26 @@
 package generics;
 
+/*
+ * Generics are a way to define a placeholder for a 
+ * non-primitive that can be supplied to a class/interface
+ * when creating an object.
+ * 
+ * Generics are commonly used with Datastructures.
+ * 
+ * Below, "Placeholder" is a generic declaration for the
+ * class "GenericClass".
+ * 
+ * Generics use 'diamond' notation : i.e. '< >'
+ * Classes:
+ * [access_mod] class [ClassName]<Generic> {
+ *     // class body... 
+ * }
+ * 
+ * Interfaces:
+ * [access_mod] interface [InterfaceName]<Generic> {
+ *     // interface body... 
+ * }
+ */
 public class GenericClass<Placeholder> {
 	private Placeholder data;
 	
