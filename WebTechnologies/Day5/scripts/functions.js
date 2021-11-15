753908d0d06279fbeec50c3fe50d2ad1d3643cb9
jhigherevature <71463449+jhigherevature@users.noreply.github.com>
2021-11-15 10:34:16 -0600

    Update functions.js

--- WebTechnologies/Day5/scripts/functions.js
+++ WebTechnologies/Day5/scripts/functions.js
@@ -28,6 +28,12 @@ function myFuncTwo(input) {
 }
 myFuncTwo("Revature");
 
+/*
+    Note that Javascript does not check the datatype for parameters and their arguments, and
+    as such, it is up to you as a developer to structure your code in a way that logically
+    makes sense based on the type of input your are expecting. (There are design patterns
+    that can work around this, but we will not discuss those here)
+*/
 function myFuncThree(inputOne, inputTwo, inputThree) {
     console.log("Input One was: " + inputOne);
     console.log("Input Two was: " + inputTwo);
